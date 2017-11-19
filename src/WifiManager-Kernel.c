2ba6faf0184f2fe922510c47391fad86689e76e1
whatshow <1257387641@qq.com>
2017-11-19 22:45:07 +0800

    增加头文件

--- src/WifiManager-Kernel.c
+++ src/WifiManager-Kernel.c
@@ -1,6 +1,22 @@
+/*** linux内核模块 ***/
 #include <linux/init.h>
+#include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/kernel.h>
+
+/*** 导入linux网络包 ***/
+#include <skbuff.h>
+#include <linux/ip.h>
+
+/*** 导入netfilter框架 ***/
+#include <netinet/in.h>
+#include <linux/netfilter.h>
+#include <linux/netfilter_ipv4.h>
+
+/*** 文件操作 ***/
+#include <sys/stat.h>
+#include <sys/types.h>
+
 /* hello_init ---- 初始化函数，当模块装载时被调用，如果成功装载返回0 否则返回非0值 */
 static int __init hello_init(void)
 {
@@ -8,9 +24,10 @@ static int __init hello_init(void)
     return 0;
 }
 /* hello_exit ---- 退出函数，当模块卸载时被调用 */
-static void __exit hello_exit(void)
+static int __exit hello_exit(void)
 {
     printk("Out, out, brief candle\n");
+    return 0;
 }
 module_init(hello_init);
 module_exit(hello_exit);

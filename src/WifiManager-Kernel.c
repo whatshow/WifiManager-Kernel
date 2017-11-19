d4d739ef22070b11471f43efa177b8d2d9ba6ea5
whatshow <1257387641@qq.com>
2017-11-19 16:47:52 +0800

    修复至可以编译

--- src/WifiManager-Kernel.c
+++ src/WifiManager-Kernel.c
@@ -7,15 +7,11 @@ static int __init hello_init(void)
     printk("I bear a charmed life.\n");
     return 0;
 }
-
 /* hello_exit ---- 退出函数，当模块卸载时被调用 */
 static void __exit hello_exit(void)
 {
     printk("Out, out, brief candle\n");
 }
-
 module_init(hello_init);
 module_exit(hello_exit);
-
-MODULE_LICENSE("MIT");
-
+MODULE_LICENSE("GPL");

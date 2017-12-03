808b6999dcfd18f431734cc17ec4dc5c6d9992f9
whatshow <1257387641@qq.com>
2017-12-04 01:36:29 +0800

    增加禁止默认IP的钩子

--- src/WifiManager-Kernel.c
+++ src/WifiManager-Kernel.c
@@ -17,18 +17,46 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
-/* hello_init ---- 初始化函数，当模块装载时被调用，如果成功装载返回0 否则返回非0值 */
-static int __init hello_init(void)
+/* 钩子 */
+//注册的钩子
+static struct nf_hook_ops nfhoForbidDefaultIP;
+//钩子回调函数
+unsigned int hf_ForbidDefaultIP(unsigned int hooknum, struct sk_buff *skb, const struct net_device *in, const struct net_device *out, int (*okfn)(struct sk_buff *))
 {
-    printk("I bear a charmed life.\n");
+    printk(KERN_INFO "packet dropped\n");   //log to var/log/messages
+    return NF_DROP;                         //drops the packet
+}
+
+
+/* 定义初始化函数 + 退出函数 */
+/**
+ * wmk_init 初始化函数，当模块装载时被调用，如果成功装载返回0 否则返回非0值
+ */
+static int __init wmk_init(void)
+{
+    //printk("I bear a charmed life.\n");
+
+    //注册禁止默认IP的钩子
+    nfhoForbidDefaultIP.hook = hf_ForbidDefaultIP;              //function to call when conditions below met
+    nfhoForbidDefaultIP.hooknum = NF_INET_PRE_ROUTING;          //called right after packet recieved, first hook in Netfilter
+    nfhoForbidDefaultIP.pf = PF_INET;                           //IPV4 packets
+    nfhoForbidDefaultIP.priority = NF_IP_PRI_FIRST;             //set to highest priority over all other hook functions
+    nf_register_hook(&nfhoForbidDefaultIP);                     //register hook
     return 0;
 }
-/* hello_exit ---- 退出函数，当模块卸载时被调用 */
-static int __exit hello_exit(void)
+/**
+ * wmk_exit 退出函数，当模块卸载时被调用
+ */
+static void __exit wmk_exit(void)
 {
-    printk("Out, out, brief candle\n");
-    return 0;
+    //取消禁止默认IP的钩子
+    nf_unregister_hook(&nfhoForbidDefaultIP);
+    //printk("Out, out, brief candle\n");
 }
-module_init(hello_init);
-module_exit(hello_exit);
-MODULE_LICENSE("GPL");
+
+//注册加载启动函数
+module_init(wmk_init);
+//注册关闭回调函数
+module_exit(wmk_exit);
+//定义支持协议
+MODULE_LICENSE("MIT");
